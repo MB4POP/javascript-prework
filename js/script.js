9d0f10625065e70bf9f475dea18d9a5e077005c3
Mariusz Białecki <mariusz.bialecki@onet.pl>
2019-08-01 11:48:56 +0200

    add js games scores

--- js/script.js
+++ js/script.js
@@ -40,3 +40,23 @@ if(playerInput == '1'){
 }
 
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+	printMessage('Ty wygrywasz!');
+  } else if ( computerMove == 'kamień' && playerMove == 'nożyce'){
+	printMessage('Przegrywasz!');
+  } else if( computerMove == 'kamień' && playerMove == 'kamień'){
+	printMessage('Mamy remis!');	
+  } else if ( computerMove == 'papier' && playerMove == 'nożyce'){
+	printMessage('Ty wygrywasz!');
+  } else if( computerMove == 'papier' && playerMove == 'kamień'){
+	printMessage('Przegrywasz!');
+  } else if ( computerMove == 'papier' && playerMove == 'papier'){
+	printMessage('Mamy remis!');
+  } else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+	printMessage('Ty wygrywasz!');
+  } else if ( computerMove == 'nożyce' && playerMove == 'nożyce'){
+	printMessage('Mamy remis!');
+  } else if( computerMove == 'nożyce' && playerMove == 'papier'){
+	printMessage('Przegrywasz!');
+  } else printMessage('Wybierz jedną z cyfr: 1, 2 lub 3!');
